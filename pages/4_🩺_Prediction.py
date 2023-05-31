a2d6b80d9d95466f7dab33eb8e7ce80e13115a6a
MehediHasanTutul <88667932+MehediHasanTutul@users.noreply.github.com>
2023-05-31 10:07:41 +1000

    added files

---
+++ pages/4_🩺_Prediction.py
@@ -0,0 +1,61 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My App", layout="wide")#centered
+
+
+col1, col2 = st.columns([1.2,5])
+
+with col1:
+    with st.container():
+            
+        st.radio(
+            'Select model:',
+            ["Victoria", "UK" ,"Canada"],
+            horizontal=False,
+        )
+    st.markdown('**Set inputs:**')
+    
+    st.radio(
+        "Unintetional weight loss 🧍‍♀️",
+        ["Yes", "No" ],
+        
+        horizontal=True,
+    )
+    st.radio(
+        "Reduced food intake 🥪",
+        ["Yes", "No" ],
+        
+        horizontal=True,
+    )
+    st.radio(
+        "Low BMI 🕴️",
+        ["Yes", "No" ],
+        
+        horizontal=True,
+    )
+    st.radio(
+        "Reduced mussle mass 🧍‍♀️",
+        ["Yes", "No" ],
+        key="visibility",
+        horizontal=True,
+    )
+    st.radio(
+        "Inflamation 🧍‍♀️",
+        ["Yes", "No" ],
+        key="visibility",
+        horizontal=True,
+    )
+    st.radio(
+        "Gender 🧍‍♀️",
+        ["Yes", "No" ],
+        key="visibility",
+        horizontal=True,
+    )
+
+    st.slider("Age 🧍‍♀️", min_value=1, max_value=100, value=50, step=1)
+
+
+
+
+
