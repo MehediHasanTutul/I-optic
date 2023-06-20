9005dc7e834c1f7f028458cf82443634eec080fb
MehediHasanTutul <88667932+MehediHasanTutul@users.noreply.github.com>
2023-06-20 21:42:05 +1000

    Update 3_🩺_Prediction.py

--- pages/3_🩺_Prediction.py
+++ pages/3_🩺_Prediction.py
@@ -123,7 +123,7 @@ else:
 # load the model from disk
 loaded_model = pickle.load(open(filename, 'rb'))
 # pred = loaded_model.predict(df.values)
-pred = loaded_model.predict_proba(df)
+pred = loaded_model.predict_proba(df.values)
 st.markdown("============================")
 if pred.argmax()==0:
     st.error(f"Predicted status after 1 year : **Deceased** (with {float(pred.max()*100):.2f}% probability)")
