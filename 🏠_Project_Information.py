5fa952a6a3581d1896b79be7954c9b4e67fbd622
MehediHasanTutul <88667932+MehediHasanTutul@users.noreply.github.com>
2023-06-21 10:25:49 +1000

    Update 🏠_Project_Information.py

--- 🏠_Project_Information.py
+++ 🏠_Project_Information.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-# Define the options
-options = ['Option 1', 'Option 2', 'Option 3', 'Option 4']
-
-# Create the toggle buttons
-selected_options = st.multiselect('Select Options', options)
-
-# Display the selected options
-st.write('Selected Options:', selected_options)
+st.markdown("Cancer-related malnutrition occurs in approximately 30% of people with cancer, depending on cancer diagnosis, disease stage and treatment type (1). Malnutrition has severe negative health consequences for people with cancer including poorer quality of life, reduced ability to complete cancer treatment and increased risk of mortality (1-3).  
+The Global Leadership Initiative on Malnutrition (GLIM) has developed a consensus for the diagnosis of malnutrition including the presence of both phenotypic and etiological criteria (4). To diagnose malnutrition, at least one phenotypic criteria (unintentional weight loss, low body mass index or reduced muscle mass) and one etiologic criteria (reduced food intake or assimilation or inflammation) must be met. The GLIM consortium recommend validation of the criteria, including studies that investigate which combinations of the criteria are most predictive of adverse outcomes to aid in identifying and intervening in patients at high risk (5). Furthermore, determining clinical and demographic risk factors for adverse outcomes can enable the identification of patients who may benefit from early and more aggressive nutrition intervention.
+Establishing which patients are at greatest risk of malnutrition-related adverse events will allow for clinical prioritisation of care and timely interventions to improve patient outcomes. Therefore, this project aims to develop an intelligent online platform, using decision trees, to predict those at risk of adverse outcomes associated with cancer-related malnutrition. ")
