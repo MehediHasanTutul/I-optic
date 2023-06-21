d9070a4e24f25338f8e49c02aec65d38e5b475ad
MehediHasanTutul <88667932+MehediHasanTutul@users.noreply.github.com>
2023-06-21 10:38:59 +1000

    Update 🏠_Project_Information.py

--- 🏠_Project_Information.py
+++ 🏠_Project_Information.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
+# im = Image.open('icon.jpg')
+st.set_page_config(
+    page_title="I-OPTIC PROJECT",
+    # page_icon=im,
+    layout="wide",
+)
+
 st.markdown('**Basic information about the project** ')
