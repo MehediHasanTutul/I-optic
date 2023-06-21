1f60f9933e1572d06aeeea21ba858365e7822acb
MehediHasanTutul <88667932+MehediHasanTutul@users.noreply.github.com>
2023-06-21 11:18:36 +1000

    Update 1_📊_Dataset_Information.py

--- pages/1_📊_Dataset_Information.py
+++ pages/1_📊_Dataset_Information.py
@@ -5,6 +5,15 @@ import numpy as np
 
 
 st.set_page_config(page_title="I-OPTIC PROJECT", layout="wide")#centered
+
+st.markdown(
+     """
+     <style>
+     #MainMenu {visibility: hidden;}
+     footer {visibility: hidden;}
+        </style>
+        """,
+        unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center'>Dataset Information</h2>", unsafe_allow_html=True)
 
 col1, col2 = st.columns([1,5])
